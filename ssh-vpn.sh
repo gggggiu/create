216df8b4b6459b4a7c517d193a307e4d35e4fcfd
khocengstore <90542418+gggggiu@users.noreply.github.com>
2023-04-05 00:10:54 +0700

    Upda

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -88,7 +88,7 @@ apt -y install nginx
 cd
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://autosc.faibervpn.xyz/nginx.conf"
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/gggggiu/create/main/nginx.conf"
 mkdir -p /home/vps/public_html
 wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/gggggiu/create/main/vps.conf"
 /etc/init.d/nginx restart
