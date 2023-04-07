e97618b7d6400283d6661430b9b86fe7b78bb7ec
khocengstore <90542418+gggggiu@users.noreply.github.com>
2023-04-07 12:52:03 +0700

    Updt

--- set-br.sh
+++ set-br.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl https://rclone.org/install.sh | bash
 printf "q\n" | rclone config
-wget -O /root/.config/rclone/rclone.conf "https://autosc.faibervpn.xyz/rclone.conf"
+wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/gggggiu/create/main/rclone.conf"
 git clone  https://github.com/magnific0/wondershaper.git
 cd wondershaper
 make install
@@ -26,12 +26,12 @@ logfile ~/.msmtp.log
 EOF
 chown -R www-data:www-data /etc/msmtprc
 cd /usr/bin
-wget -O autobackup "https://autosc.faibervpn.xyz/autobackup.sh"
-wget -O backup "https://autosc.faibervpn.xyz/backup.sh"
-wget -O bckp "https://autosc.faibervpn.xyz/bckp.sh"
-wget -O restore "https://autosc.faibervpn.xyz/restore.sh"
-wget -O strt "https://autosc.faibervpn.xyz/strt.sh"
-wget -O limit-speed "https://autosc.faibervpn.xyz/limit-speed.sh"
+wget -O autobackup "https://raw.githubusercontent.com/gggggiu/create/main/autobackup.sh"
+wget -O backup "https://raw.githubusercontent.com/gggggiu/create/main/backup.sh"
+wget -O bckp "https://raw.githubusercontent.com/gggggiu/create/main/bckp.sh"
+wget -O restore "https://raw.githubusercontent.com/gggggiu/create/main/restore.sh"
+wget -O strt "https://raw.githubusercontent.com/gggggiu/create/main/strt.sh"
+wget -O limit-speed "https://raw.githubusercontent.com/gggggiu/create/main/limit-speed.sh"
 chmod +x autobackup
 chmod +x backup
 chmod +x bckp
